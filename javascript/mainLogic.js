e59bb139ee58399ad4c16a05831a76fbae445ffc
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2019-11-21 15:51:10 +0200

    create new file requests-http and added fetchData method

--- javascript/mainLogic.js
+++ javascript/mainLogic.js
@@ -2,6 +2,11 @@ import * as COMPONENTS from './elements.js';
 
 COMPONENTS.SEARCH_BUTTON.addEventListener('click', searchWeather);
 
+
 function searchWeather(){
-    console.log("Click!")
+    const CITY_NAME = COMPONENTS.SEARCHED_CITY.value.trim();
+    if(CITY_NAME.length === 0){
+        return alert('Please enter a city name!')
+    }
+    console.log(`Click! You entered ${CITY_NAME}`);
 }
