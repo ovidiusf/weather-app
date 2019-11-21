afb69702d8becd4f1946b7bc5a42963462306270
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2019-11-21 13:47:20 +0200

    add elements.js file which contains all dom elements required

---
+++ javascript/elements.js
@@ -0,0 +1,10 @@
+// exporting the DOM elements that we need for the app
+export const SEARCH_BUTTON = document.querySelector('button');
+export const SEARCHED_CITY = document.querySelector('#city');
+
+export const LOADING_TEXT_ELEMENT = document.querySelector('#load');
+export const WEATHER_BOX = document.querySelector('#weather');
+
+export const WEATHER_CITY = WEATHER_BOX.firstElementChild;
+export const WEATHER_DESCRIPTION = document.querySelector('#weatherDescription');
+export const WEATHER_TEMPERATURE = document.querySelector('#weatherTemperature');
